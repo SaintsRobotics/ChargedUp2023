989d937b471da417b776cced6dc9276b2df6a60a
ProgrammingSR <programming@saintsrobotics.com>
2023-03-10 18:12:29 -0800

    chore: untested changed elevator constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -31,20 +31,20 @@ public final class Constants {
     public static final double kPivotEncoderOffset = 168;
     public static final double kElevatorEncoderPositionConversionFactor = 0.035;
 
-    public static final double kElevatorStartingPosition = 0;
-    public static final double kElevatorLowPosition = 0.159;
-    public static final double kElevatorHighPosition = 0.780;
+    public static final double kElevatorStartingPosition = 1.219;
+    public static final double kElevatorLowPosition = 1.378;
+    public static final double kElevatorHighPosition = 1.999;
 
     public static final double kPivotMaxPosition = 87;
     public static final double kPivotMinPosition = 12;
 
     /** Maximum distance the elevator can extend before hitting the stop. */
-    public static final double kElevatorMaxPosition = 0.73;
-    public static final double kElevatorMinPosition = 0.2;
+    public static final double kElevatorMaxPosition = 1.949;
+    public static final double kElevatorMinPosition = 1.419;
 
     /** Maximum height legal for the game. */
-    public static final double kMaxGameHeight = 0.5;
-    public static final double kMaxGameExtension = 0.4;
+    public static final double kMaxGameHeight = 1.727;
+    public static final double kMaxGameExtension = 1.778;
 
     public static final double kPivotFeedForwardCoefficient = 0.03;
     public static final double kElevatorFeedForwardCoefficient = 0.05;
